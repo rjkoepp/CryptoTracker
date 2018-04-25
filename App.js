10bbc63a1bacbc70689f421943b1a3a5bc6a782a
rjkoepp <rchangko@ucsd.edu>
2018-04-24 22:27:20 -0700

    starting to incorporate redux

---
+++ App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import {View} from 'react-native';
+import {Header} from './src/components';
+
+// https://medium.com/react-native-training/bitcoin-ripple-ethereum-price-checker-with-react-native-redux-e9d076037092
+
+export default class App extends React.Component {
+  render() {
+    return (
+
+      <View>
+
+        <Header/>
+
+      </View>
+    );
+  }
+}
